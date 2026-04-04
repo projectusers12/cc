a321d2bbcd28dd18124b04e435d8cc9546367692
projectusers12 <projectusers12@gmail.com>
2026-04-04 09:03:51 +0530

    Update java.py

--- java.py
+++ java.py
@@ -1,1 +1,34 @@
 
+<dependency>
+    <groupId>com.sun.xml.ws</groupId>
+    <artifactId>jaxws-rt</artifactId>
+    <version>2.3.3</version>
+</dependency>
+
+
+package com.my.prac;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+
+@WebService
+public class HelloService {
+    @WebMethod
+    public String sayHello(String name) {
+        return "Hello " + name + " ! ";
+    }
+}
+
+
+package com.my.prac;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+
+@WebService
+public class MathService {
+    @WebMethod
+    public String getSquareAndCube(int num) {
+        int sq = num * num;
+        int cb = num * num * num;
+        return "Square: " + sq + " | Cube: " + cb;
+    }
+}
